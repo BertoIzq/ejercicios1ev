3255eb25bf9dc9aab1b6afba47b2d253fa30d1c1
Berto <Berto@BERTO-IZQ>
2022-06-13 19:28:38 +0200

    Ejercicio 1, unidad 2

---
+++ src/unidad2/Division.java
@@ -0,0 +1,13 @@
+package unidad2;
+
+public class Division {
+
+	public static void main(String[] args) {
+		double dividendo, divisor, cociente;
+		dividendo = 1234.;
+		divisor = 532.;
+		cociente = dividendo / divisor;
+		System.out.printf("\n%15.2f", cociente);
+	}
+
+}
