93a5db3207fea94133619bf57cf17d3c52941c19
Berto <Berto@BERTO-IZQ>
2022-06-15 18:37:55 +0200

    Ejercicio 1, unidad 3

---
+++ src/unidad3/ParImpar.java
@@ -0,0 +1,17 @@
+package unidad3;
+
+import java.util.*;
+
+public class ParImpar {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Introduzca numero: ");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		if (num%2. == 0)	System.out.println("El numero es par");
+		else 				System.out.println("El numero es impar");
+
+	}
+
+}
